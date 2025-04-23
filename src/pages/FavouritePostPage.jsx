9221a703d0a13052637d3e923950a44bbb72930e
george keli <keligeorge558@gmail.com>
2025-04-23 09:25:49 +0300

    update

--- src/pages/FavouritePostPage.jsx
+++ src/pages/FavouritePostPage.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
 
 const FavoritePosts = () => {
   const [favoritePosts, setFavoritePosts] = useState([]);
