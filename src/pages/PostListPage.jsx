ab92e31d226478c1c4e953a44b2a45ea3ad2eac9
Joram Wayne <joramwmuya@gmail.com>
2025-04-24 10:07:46 +0300

    Working-progress

--- src/pages/PostListPage.jsx
+++ src/pages/PostListPage.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import './PostListPage.css'; 
+
 
 function PostListPage() {
   const [posts, setPosts] = useState([]);
