18246cfd86f28826fe2ae672c1ccc726de816a79
Joram Wayne <joramwmuya@gmail.com>
2025-06-25 11:09:42 +0300

    Configuration

--- social-media-mvp-python/app/app.py
+++ social-media-mvp-python/app/app.py
@@ -3,7 +3,8 @@ from models import db, User, Post, Favorite, BlockedPost
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config("SQLALCHEMY_DATABASE_URI") = "sqlite"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
