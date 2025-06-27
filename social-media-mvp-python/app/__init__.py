6684932b18a71ae4115c4c6593c72abd5821d9ef
Joram Wayne <joramwmuya@gmail.com>
2025-06-27 12:58:43 +0300

    Deploying

--- social-media-mvp-python/app/__init__.py
+++ social-media-mvp-python/app/__init__.py
@@ -1,27 +1,28 @@
+# app/__init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-from app.config import Config
-
 
 db = SQLAlchemy()
 migrate = Migrate()
 jwt = JWTManager()
 
-def create_app(config_class=Config):
+def create_app():
     app = Flask(__name__)
-    app.config.from_object(config_class)
-
+    app.config.from_object('config.Config') 
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
-    CORS(app)
 
-    from .routes import bp as api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
+    CORS(
+        app,
+        resources={r"/api/*": {"origins": "*"}},
+        send_wildcard=True,
+        always_send=True
+    )
+    from .routes import bp as routes_bp
+    app.register_blueprint(routes_bp, url_prefix='/api')
 
-
-    from app import models  
     return app
